db090ee9089877dcad3a153434ffb1ad584c4360
Parvej101 <you@example.com>
2024-10-22 09:55:39 +0600

    setup react and tailwind and also json data

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Blogs from './assets/components/header/Blogs/Blogs'
+import Header from './assets/components/header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
